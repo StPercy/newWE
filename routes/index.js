8088cec01b7d57a86c54b4590c25bdf1d4a9c04d
Stefan Pertschi <stefan.pertschi@gfn.de>
2024-03-26 14:07:52 +0100

    fixed Uebung 29

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index', { title: 'Express' })
+    //res.render('index', { title: 'Express', words: ['EJS', 'is', '<span style="font-weight: bold">awesome</span>']  })
+    res.render('index', { title: 'Express',
+        content: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+    })
 })
 
 module.exports = router

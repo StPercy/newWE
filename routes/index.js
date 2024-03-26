cd85c12ba9df2fa0c86deb2fd49d730a0b8e44e1
Stefan Pertschi <stefan.pertschi@gfn.de>
2024-03-26 14:49:42 +0100

    absolved training

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
-const express = require('express')
+var express = require('express')
+var router = express.Router()
 
-const router = express.Router()
+router.get('/', function (req, res, next) {
+    res.render('index')
+})
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-    //res.render('index', { title: 'Express', words: ['EJS', 'is', '<span style="font-weight: bold">awesome</span>']  })
-    res.render('index', { title: 'Express',
-        content: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-    })
+router.get('/addList', function (req, res, next) {
+    res.render('addList')
 })
 
 module.exports = router

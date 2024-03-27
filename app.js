e027fc02a9a655e1fde25b8bbac9583741a89a0b
Stefan Pertschi <stefan.pertschi@gfn.de>
2024-03-27 11:08:00 +0100

    finished part5_2

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const apiRouter = require('./routes/api/shoppingLists')
 
 const app = express()
 
+app.locals.root = __dirname
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')

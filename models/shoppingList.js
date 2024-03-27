285ec47ec822ff9857f1a0e88897b9ed2eef938e
Stefan Pertschi <stefan.pertschi@gfn.de>
2024-03-27 11:52:13 +0100

    5_3 ongoing

--- models/shoppingList.js
+++ models/shoppingList.js
@@ -6,8 +6,7 @@ const shoppingListSchema = new Schema({
         required: true,
         immutable: true,
         validate: {
-            validator: (value) => Number.isInteger(Number(value)) 
-            //&& value.length === 6,
+            validator: (value) => Number.isInteger(Number(value)) && value.length === 6,
         },
     },
     entries: [
